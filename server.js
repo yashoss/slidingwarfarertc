1299a3d7eac7bf5b7dc3c66532edcc60711df63f
yasin <yashoss@gmail.com>
2017-06-22 16:07:32 -0700

    Added express server to host on heroku

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 9090;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+
 //require our websocket library
-var WebSocketServer = require('ws').Server;
+// var WebSocketServer = require('ws').Server;
 
 //creating a websocket server at port 9090
-var wss = new WebSocketServer({port: 9090});
+// var wss = new WebSocketServer({port: 9090});
 
 //all connected to the server users
 var users = {};
