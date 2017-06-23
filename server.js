9440d8fe4b57f51770666284d255e0f18936780b
yasin <yashoss@gmail.com>
2017-06-23 12:29:50 -0700

    Connecting websockets error

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const server = express();
+
+// server.use(express.static(__dirname));
+server.use(express.static(path.join(__dirname, 'lib')));
+server.use('/assets', express.static(path.join(__dirname, 'assets')));
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
 
